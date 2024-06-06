ca86ac42bce2277cb171e468b600f4bd5b9f28b5
Anas <anas.001falaki@gmail.com>
2024-06-06 12:26:35 +0100

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to compute the floor of a float.
+"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Compute the floor of a float and return it as an integer.
+
+    Args:
+        n (float): The float number to floor.
+
+    Returns:
+        int: The floor of the float number.
+    """
+
+    return math.floor(n)
