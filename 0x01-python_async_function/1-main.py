de693ab578c2c49e92e3e174451f0d233a20ece4
Anas <anas.001falaki@gmail.com>
2024-06-14 16:22:11 +0100

    run multiple wait_random

--- 0x01-python_async_function/1-main.py
+++ 0x01-python_async_function/1-main.py
@@ -8,7 +8,6 @@ Test file for printing the correct output of the wait_n coroutine.
 import asyncio
 from concurrent_coroutines import wait_n
 
-
 if __name__ == "__main__":
     print(asyncio.run(wait_n(5, 5)))
     print(asyncio.run(wait_n(10, 7)))
