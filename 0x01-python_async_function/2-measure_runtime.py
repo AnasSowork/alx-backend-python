de82c8c4cdea24dace7cc95f6fe1e38873d088cd
Anas <anas.001falaki@gmail.com>
2024-06-14 16:43:18 +0100

    update

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -24,13 +24,14 @@ def measure_time(n: int, max_delay: int) -> float:
         float: Average execution time per call of wait_n.
     """
     start_time = time.perf_counter()
-    run(wait_n(n, max_delay))  # Run wait_n asynchronously and wait for completion
+    run(wait_n(n, max_delay))  # Run wait_n asynchronously and wait
     end_time = time.perf_counter()
 
     total_time = end_time - start_time
     avg_time_per_call = total_time / n
 
     return avg_time_per_call
+
 
 # Example usage
 if __name__ == "__main__":
