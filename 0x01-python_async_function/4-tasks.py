fb8491d31c8068f269b5fbf1f63d4a80effd4002
Anas <anas.001falaki@gmail.com>
2024-06-14 17:05:37 +0100

    tasks 2

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Module containing functions to create asyncio.Task objects
+for concurrent coroutines.
+"""
+
+import asyncio
+from typing import List
+from basic_async_syntax import wait_random
+
+
+async def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Creates and returns an asyncio.Task for the wait_random
+    coroutine with the specified max_delay.
+
+    Args:
+        max_delay (int): The maximum delay time in seconds.
+
+    Returns:
+        asyncio.Task: The asyncio.Task object associated with
+        wait_random.
+    """
+    return asyncio.create_task(wait_random(max_delay))
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawns task_wait_random n times with the specified max_delay
+    and returns
+    the list of delays in ascending order.
+
+    Args:
+        n (int): The number of times to spawn task_wait_random.
+        max_delay (int): The maximum delay in seconds for each
+        task_wait_random.
+
+    Returns:
+        List[float]: List of delays in ascending order.
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
